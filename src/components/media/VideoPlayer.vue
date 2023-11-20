3821be562438cc898e8005549d96539be0979628
Lucas <lucasacuadra@gmail.com>
2023-11-20 11:12:56 +0100

    fix video player size

--- src/components/media/VideoPlayer.vue
+++ src/components/media/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="my-auto">
-		<video ref="videoPlayer" width="640" height="460" controls>
+		<video ref="videoPlayer" class="w-auto h-auto mx-10" controls>
 			<source :src="video" type="video/mp4" />
 		</video>
 	</div>
